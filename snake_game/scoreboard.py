6f3fee6499ac737d3402a4cca3c111e517be9ca1
HaarishaH <137532846+HaarishaH@users.noreply.github.com>
2024-11-19 18:14:49 +0530

    Create scoreboard.py

---
+++ snake_game/scoreboard.py
@@ -0,0 +1,26 @@
+
+scoreboard.py
+from turtle import Turtle
+
+updates = False
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.current_score = 0
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score : {self.current_score}", align="left", font=("Arial ", 12, "normal"))
+
+
+    def add_score(self):
+        self.current_score += 1
+        self.update_scoreboard()
+        global updates
+        updates = True
